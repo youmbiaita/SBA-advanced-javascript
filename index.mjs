7183a4e741cc1cc2b11c790d5f527714c202f88e
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-05-14 23:59:12 -0400

    added changes

--- index.mjs
+++ index.mjs
@@ -3,12 +3,15 @@
 import { fetchData, postData } from './apiCall.mjs';
 import { renderGallery, renderError } from './other.mjs';
 
-const API_KEY = "live_wyM7IRDoOf36EVWhNv3v371zpc0H1osTq5P5XGEX2GmnoJEayuYwQfwhhCgqnyOY";
+//const API_KEY = "live_wyM7IRDoOf36EVWhNv3v371zpc0H1osTq5P5XGEX2GmnoJEayuYwQfwhhCgqnyOY";
+const API_KEY = "live_siklOA1eZTT1V7XSk3Ud2pumwWl0tZNI71VX3SMBdvXZMNcqTxNm0YqwRlnqjUjA";
 const API_URL = " https://api.thecatapi.com/v1";
 
-async function searchCats(query, page = 1) {
+async function searchCats(query, page = 1, limit = 10, hasBreeds = 0) {
+    const url = `${API_URL}/images/search?limit=${limit}&page=${page}&q=${query}&has_breeds=${hasBreeds}&api_key=${API_KEY}`;
+    console.log(url)
     try {
-        const data = await fetchData(`${API_URL}/images/search?limit=10&page=${page}&q=${query}&api_key=${API_KEY}`);
+        const data = await fetchData(url);
         renderGallery(data);
     } catch (error) {
         renderError(error);
@@ -27,7 +30,10 @@ async function vote(imageId, voteType) {
 document.getElementById('search-form').addEventListener('submit', function (event) {
     event.preventDefault();
     const query = document.getElementById('search-input').value;
-    searchCats(query);
+    const limit = parseInt(document.getElementById("limit").value);
+    const page = parseInt(document.getElementById("page").value);
+    const hasBreeds = document.getElementById("has-breeds").value === "on" ? 1 : 0;
+    searchCats(query, page, limit, hasBreeds);
 });
 
 document.getElementById('gallery').addEventListener('click', function (event) {
